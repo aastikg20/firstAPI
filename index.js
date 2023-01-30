4ececea55e199eba1efbea4fd9df0127a2825787
Aastik <aastikg20@iitk.ac.in>
2023-01-31 01:35:58 +0530

    change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongoString = process.env.DATABASE_URL
+mongoose.set("strictQuery", true);
 mongoose
   .connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
