be1aa54705dfa4f70a9d4af82dd263bd584d35a8
Aastik <aastikg20@iitk.ac.in>
2023-02-16 14:27:56 +0530

    Added Date and Time

--- model/user.js
+++ model/user.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({
     tid : String,
     epc : String,
-    user : String
+    user : String,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
   });
 
   module.exports = mongoose.model('RFIDdata', dataSchema)
